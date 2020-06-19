e1e9278216d5d4fe89be18a8190a0464af378c56
Jenifer Placido <jenifer.sdti@gmail.com>
2020-06-19 20:13:31 -0300

    atividade1

--- atividade_1/index.js
+++ atividade_1/index.js
@@ -1,5 +1,11 @@
-// ## Importe a biblioteca ##
+let rs = require ("readline-sync")
 
+let numVoo = rs.question ("Seja Bem Vindo a Arbyte Airlines, qual o numero do seu voo?")
 
+let fileira = rs.question ("Em qual fileira voce deseja sentar?")
 
-// ## Faça o código ##
+console.log ("O seu Cartão de embarque para o voo:" +numVoo)
+
+console.log ("Você sentara na fileira:" +numVoo)
+
+console.log ("Muito Obrigado por voar com a Airbyte Airlines!!!")
