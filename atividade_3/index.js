c70c037db3b3536ecd839930108ef95d752eee7f
Jenifer Placido <jenifer.sdti@gmail.com>
2020-06-22 17:52:34 -0300

    Atividade3

--- atividade_3/index.js
+++ atividade_3/index.js
@@ -1,5 +1,8 @@
-// ## Importe a biblioteca ##
+let rs = require ("readline-sync")
 
+let quad = rs.question ("Seja Bem Vindo a Arbyte Calculos, qual o tamanho dos lados do quadrado?")
 
+let resultado = (quad*quad)
 
-// ## Faça o código ##
+console.log ("O resultado da area do quadrado é:" +resultado)
+
