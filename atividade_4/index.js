402bed34955feb5430f7e51632f41354a040073e
Jenifer Placido <jenifer.sdti@gmail.com>
2020-06-22 18:40:58 -0300

    Atividade4

--- atividade_4/index.js
+++ atividade_4/index.js
@@ -1,5 +1,102 @@
-// ## Importe a biblioteca ##
+let rs = require ("readline-sync")
 
+let mes = rs.question ("Seja Bem Vindo a Arbyte Signos, qual o mês do seu nascimento?")
 
+let dia = rs. question ("Qual o dia do seu nascimento?")
 
-// ## Faça o código ##
+if(mes == 1){
+    if(dia <=20){
+        console.log ("O Seu Signo é Capricornio, pessoas de Capricornio são maduras e Bem Humoradas")
+    }
+}
+    else {
+        console.log ("O Seu Signo é Aquario, pessoas de aquario são solidarias e bem humanas")
+    }
+         if(mes == 2){
+    if(dia <=18){
+        console.log ("O Seu Signo é Aquario, pessoas de aquario são solidarias e bem humanas")
+    }
+    else{
+        console.log ("O Seu signo é de Peixes, pessoas de peixe são compassivos e inspirador")
+    }
+    }
+    else if(mes == 3){
+        if(dia <=20){
+            console.log ("O Seu signo é de Peixes, pessoas de peixe são compassivos e inspirador")
+        }
+        else{
+            console.log ("O Seu signo é de Aries, pessoas de aries são lideres e empreendedoras")
+        }
+    }
+    else if(mes == 4){
+        if(dia <=19){
+            console.log ("O Seu signo é de Aries, pessoas de aries são lideres e empreendedoras")
+        }
+        else{
+            console.log ("O Seu signo é de Touro, pessoas de touro são persistentes e teimosas")
+        }
+    }
+    else if(mes == 5){
+        if(dia <=20){
+            console.log ("O Seu signo é de Touro, pessoas de touro são persistentes e teimosas")
+        }
+        else{
+            console.log ("O Seu signo é de Gemeos, pessoas de gemeos são curiosas e tem muita carisma")
+        }
+    }
+    else if(mes == 6){
+        if(dia <=21){
+            console.log ("O Seu signo é de Gemeos, pessoas de gemeos são curiosas e tem muita carisma")
+        }
+        else{
+            console.log ("O Seu signo é de Cancer, pessoas de cancer são carinhosas e muito familia")
+        }
+    }
+    else if(mes == 7){
+        if(dia <=22){
+            console.log ("O Seu signo é de Cancer, pessoas de cancer são carinhosas e muito familia")
+        }
+        else{
+            console.log ("O Seu signo é de Leao, pessoas de leao tem autoestima e criatividade")
+        }
+    }
+    else if(mes == 8){
+        if(dia <=23){
+            console.log ("O Seu signo é de Leao, pessoas de leao tem autoestima e criatividade")
+        }
+        else{
+            console.log ("O Seu signo é de Virgem, pessoas de virgem são objetivas e organizadas")
+        }
+    }
+    else if(mes == 9){
+        if(dia <=22){
+            console.log ("O Seu signo é de Virgem, pessoas de virgem são objetivas e organizadas")
+        }
+        else{
+            console.log ("O Seu signo é de Libra, pessoas de libra tem empatia e vaidade")
+        }
+    }
+    else if(mes == 10){
+        if(dia <=22){
+            console.log ("O Seu signo é de Libra, pessoas de libra tem empatia e vaidade")
+        }
+        else{
+            console.log ("O Seu signo é de Escorpião, pessoas de escorpião são ciumentas e muito intenso")
+        }
+    }
+    else if(mes == 11){
+        if(dia <=22){
+            console.log ("O Seu signo é de Escorpião, pessoas de escorpião são ciumentas e muito intenso")
+        }
+        else{
+            console.log ("O Seu signo é de Sagitario, pessoas de sagitario são otimistas e jovial")
+        }
+    }
+    else if(mes == 12){
+        if(dia <=21){
+            console.log ("O Seu signo é de Sagitario, pessoas de sagitario são otimistas e jovial")
+        }
+        else{
+            console.log ("O Seu Signo é Capricornio, pessoas de Capricornio são maduras e Bem Humoradas")
+        }
+    }
